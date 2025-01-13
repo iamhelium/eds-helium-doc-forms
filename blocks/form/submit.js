a6997a8ba77f7c9720820cbb8fbe7db47c45dc4f
Surabhi-helium <surabhi@iamhelium.com.au>
2025-01-13 14:54:13 +0530

    form

--- blocks/form/submit.js
+++ blocks/form/submit.js
@@ -84,7 +84,7 @@ async function prepareRequest(form) {
   let url;
   let baseUrl = getSubmitBaseUrl();
   if (!baseUrl && org && site) {
-    baseUrl = 'https://main--eds-helium-doc-forms--iamhelium.hlx.page/';
+    baseUrl = 'https://forms.adobe.com/adobe/forms/af/submit/';
     headers['x-adobe-routing'] = `tier=${tier},bucket=${branch}--${site}--${org}`;
     url = baseUrl + btoa(form.dataset.action);
   } else {
